b4f74f33e0c2546c57354d0911f2283ec20a6b88
luudd <112076286+luudd@users.noreply.github.com>
2023-09-14 01:24:38 +0800

    update

--- lc4.py
+++ lc4.py
@@ -5,3 +5,8 @@ class Solution:
             char_value = ord(columnTitle[len(columnTitle) - 1 - i]) - ord('A') + 1
             result += char_value * (26 ** i)
         return result
+
+solution = Solution()
+print(solution.titleToNumber("A"))  # Output: 1
+print(solution.titleToNumber("AB"))  # Output: 28
+print(solution.titleToNumber("ZY"))  # Output: 701
